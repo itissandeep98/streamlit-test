dcf1551e1b7679dd4865f28c15bc3cff417617ed
itissandeep98 <itissandeep98@gmail.com>
2021-02-09 20:44:33 +0530

    registration form

--- Readme.md
+++ Readme.md
@@ -1,1 +1,1 @@
-![image](https://user-images.githubusercontent.com/44255731/107359381-9d65e100-6afa-11eb-9f7f-8b114aed9d05.png)
+![image](https://user-images.githubusercontent.com/44255731/107383979-897ca800-6b17-11eb-87c0-b5901bd259a8.png)
--- main.py
+++ main.py
@@ -1,114 +1,22 @@
 import streamlit as st 
 import numpy as np 
 
-import matplotlib.pyplot as plt
-from sklearn import datasets
-from sklearn.model_selection import train_test_split
+st.title("Registration form")
 
-from sklearn.metrics import accuracy_score
-from sklearn.decomposition import PCA
-from sklearn.svm import SVC
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.ensemble import RandomForestClassifier
+first,last=st.beta_columns(2)
+first.text_input("First Name")
+last.text_input("Last Name")
 
-st.title("streamlit demo")
-
-st.write('''
-# Exploring streamlit
-''')
-
-def get_dataset(name):
-    data = None
-    if name == 'Iris':
-        data = datasets.load_iris()
-    elif name == 'Wine':
-        data = datasets.load_wine()
-    else:
-        data = datasets.load_breast_cancer()
-    X = data.data
-    y = data.target
-    return X, y
+email,mob=st.beta_columns([3,1])
+email.text_input("email")
+mob.text_input("mob")
 
 
-def add_parameter_ui(clf_name):
-    params = dict()
-    if clf_name == 'SVM':
-        C = st.sidebar.slider('C', 0.01, 10.0)
-        params['C'] = C
-    elif clf_name == 'KNN':
-        K = st.sidebar.slider('K', 1, 15)
-        params['K'] = K
-    else:
-        max_depth = st.sidebar.slider('max_depth', 2, 15)
-        params['max_depth'] = max_depth
-        n_estimators = st.sidebar.slider('n_estimators', 1, 100)
-        params['n_estimators'] = n_estimators
-    return params
+user,pw,pw2=st.beta_columns(3)
+user.text_input("Username")
+pw.text_input("Password",type="password")
+pw2.text_input("Confirm password",type="password")
 
-dataset_name=st.sidebar.selectbox("Select Dataset",("Iris","Wine","Breast Cancer"))
-X, y = get_dataset(dataset_name)
-st.write('Shape of dataset:', X.shape)
-st.write('number of classes:', len(np.unique(y)))
-
-classifier_name = st.sidebar.selectbox(
-    'Select classifier',
-    ('KNN', 'SVM', 'Random Forest')
-)
-params = add_parameter_ui(classifier_name)
-
-
-
-def get_classifier(clf_name, params):
-    clf = None
-    if clf_name == 'SVM':
-        clf = SVC(C=params['C'])
-    elif clf_name == 'KNN':
-        clf = KNeighborsClassifier(n_neighbors=params['K'])
-    else:
-        clf = clf = RandomForestClassifier(n_estimators=params['n_estimators'], 
-            max_depth=params['max_depth'], random_state=1234)
-    return clf
-
-clf = get_classifier(classifier_name, params)
-
-
-
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
-
-clf.fit(X_train, y_train)
-y_pred = clf.predict(X_test)
-
-acc = accuracy_score(y_test, y_pred)
-
-st.write(f'Classifier = {classifier_name}')
-st.write(f'Accuracy =', acc)
-
-pca = PCA(2)
-X_projected = pca.fit_transform(X)
-
-x1 = X_projected[:, 0]
-x2 = X_projected[:, 1]
-
-fig = plt.figure()
-plt.scatter(x1, x2,
-        c=y, alpha=0.8,
-        cmap='viridis')
-
-plt.xlabel('Principal Component 1')
-plt.ylabel('Principal Component 2')
-plt.colorbar()
-
-#plt.show()
-st.pyplot(fig)
-
-
-
-
-
-
-
-
-
-
-
+ch,b1,sub=st.beta_columns(3)
+ch.checkbox("I Agree")
+sub.button("Submit")
